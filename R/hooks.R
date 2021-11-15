f08f546d04bd901da0f52a18f07d56fabad0bda5
Vincent Dorie <vdorie@gmail.com>
2021-11-15 10:53:31 -0500

    More specificity in unload

--- R/hooks.R
+++ R/hooks.R
@@ -3,7 +3,7 @@
   ## gc is necessary to collect external pointers who have not yet been collected
   ## that have finalizers pointing to the soon-to-unloaded dll
   gc(FALSE)
-  if (is.loaded("stan4bart_finalize")) {
+  if (is.loaded("stan4bart_finalize", PACKAGE = "stan4bart")) {
     .Call(C_stan4bart_finalize)
     library.dynam.unload("stan4bart", libpath)
   }
